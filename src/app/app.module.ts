8cecba1a3e3dd800e95e19488bb18ec727bacd78
kingsley <nwankwo.kingsleyjohn@gmail.com>
2017-05-26 18:03:18 -0400

    implement and test facebook login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -7,6 +7,22 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+//import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyDlN-RFjSBq4jBipkkyDYtF2AMuu8Kz1TY",
+    authDomain: "ladrope-9e888.firebaseapp.com",
+    databaseURL: "https://ladrope-9e888.firebaseio.com",
+    projectId: "ladrope-9e888",
+    storageBucket: "ladrope-9e888.appspot.com",
+    messagingSenderId: "205152875857"
+
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +30,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    //AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+//import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+displayName;
+photoURL;
 
+constructor(public navCtrl: NavController,
+    private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => {
+      if (!user) {
+        this.displayName = null;
+        this.photoURL = null
+        return;
+      }
+      this.displayName = user.displayName;  
+      this.photoURL = user.photoURL;
+    });
+  }
+
+  signInWithFacebook() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(res => console.log(res));
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
   }
 
 }
