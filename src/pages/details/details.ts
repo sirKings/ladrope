ba283e56e82dfe2384be243818415bda54917c12
Kingsley <nwankwo.kingsleyjohn@gmail.com>
2017-06-16 11:47:30 +0100

    landing page and details page

---
+++ src/pages/details/details.ts
@@ -0,0 +1,83 @@
+﻿import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { HomePage } from '../home/home';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthProvider } from '../../providers/auth/auth';
+
+import { EmailValidator } from '../../validators/email';
+
+@IonicPage()
+@Component({
+  selector: 'page-details',
+  templateUrl: 'details.html',
+})
+export class DetailsPage {
+  user;
+  uid;
+  email;
+  name;
+  photoURL;
+  userDetailsForm:FormGroup;
+
+  constructor(public navCtrl: NavController, afAuth: AngularFireAuth, public navParams: NavParams, public formBuilder: FormBuilder, public alertCtrl: AlertController, private authData: AuthProvider) {
+    const authObserver = afAuth.authState.subscribe( user => {
+      if (user) {
+        this.uid = user.uid
+        this.email = user.email;
+        this.name = user.displayName;
+        this.photoURL = user.photoURL;
+        console.log(this.email + this.name + this.photoURL)
+        authObserver.unsubscribe();
+      } else {
+        this.navCtrl.pop();
+        authObserver.unsubscribe();
+      }
+      });
+
+    this.userDetailsForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, 
+        EmailValidator.isValid])],
+      name: ['', Validators.compose([Validators.minLength(2), Validators.required])],
+      phone: ['', Validators.compose([Validators.required])],
+      gender: ['', Validators.compose([Validators.required])],
+      //height: ['', Validators.compose([Validators.required])],
+      address: ['', Validators.compose([Validators.required])]
+    });
+  }
+
+
+
+  writeUser(){
+      if (this.email){
+          this.userDetailsForm.value.email = this.email;
+      }
+
+      if (!this.photoURL){
+          this.photoURL = 'assets/images/Male-Placeholder.jpg'
+      }
+
+      if (this.name) {
+          this.userDetailsForm.value.name = this.name;
+      }
+
+      this.user = {
+        name: this.userDetailsForm.value.name,
+        email: this.userDetailsForm.value.email,
+        photoURL: this.photoURL,
+        address: this.userDetailsForm.value.address,
+        gender: this.userDetailsForm.value.gender,
+        phone: this.userDetailsForm.value.phone,
+      }
+
+      this.authData.writeUserData(this.uid, this.userDetailsForm.value.name, this.userDetailsForm.value.email, this.photoURL, this.userDetailsForm.value.address,  this.userDetailsForm.value.gender, this.userDetailsForm.value.phone);
+      this.navCtrl.setRoot(HomePage);
+  };
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DetailsPage');
+  }
+
+}
---
+++ src/pages/landing/landing.ts
@@ -0,0 +1,29 @@
+﻿import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SignupPage } from '../signup/signup';
+import { LoginPage } from '../login/login'
+
+
+@IonicPage()
+@Component({
+  selector: 'page-landing',
+  templateUrl: 'landing.html',
+})
+export class LandingPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  goToSignUp(){
+      this.navCtrl.push(SignupPage)
+  }
+
+  goToLogin(){
+      this.navCtrl.push(LoginPage)
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LandingPage');
+  }
+
+}
